8973955c40c1fc5893f31c8c9c9c44eea6ac72ec
odupree1988 <odupree1988@gmail.com>
2021-07-27 21:47:52 -0400

    CRUD routes updated

--- controllers/api/comic-routes.js
+++ controllers/api/comic-routes.js
@@ -1,14 +1,90 @@
 const router = require("express").Router();
 const { User, Comic } = require("../../models");
 
-// router
-// .get("/", (req, res) => {
-//     Comic.findAll();
-//   })
-//   .then((dbComicData) => json(dbComicData))
-//   .catch((err) => {
-//     console.log(err);
-//     res.status(500).json(err);
-//   });
+router.get("/", (req, res) => {
+  Comic.findAll()
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/:id", (req, res) => {
+  Comic.findOne({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbComicData) => {
+      if (!dbComicData) {
+        res.status(404).json({ message: "No comic exists with this id!" });
+        return;
+      }
+      res.json(dbComicData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.post("/", (req, res) => {
+  Comic.create({
+    super_hero: req.body.super_hero,
+    book_name: req.body.book_name,
+    //requires req.session.user_id
+    user_id: req.body.user_id,
+  })
+    .then((dbComicData) => res.json(dbComicData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.put("/:id", (req, res) => {
+  Comic.update(
+    {
+      super_hero: req.body.super_hero,
+      book_name: req.body.book_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((dbComicData) => {
+      if (!dbComicData) {
+        res.status(404).json({ message: "No comic exists with this id!" });
+        return;
+      }
+      res.json(dbComicData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  Comic.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbComicData) => {
+      if (!dbComicData) {
+        res.status(404).json({ message: "No comic exists with this id!" });
+        return;
+      }
+      res.json(dbComicData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
