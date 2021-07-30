b5e7aa611917b9176c24b15f96c211d27cc0adc4
Salas4969 <82052286+Salas4969@users.noreply.github.com>
2021-07-29 20:49:31 -0400

    api calls again

--- script-api.js
+++ script-api.js
@@ -1,51 +1,56 @@
 const md5 = require('md5');
- const comic_URL ='https://gateway.marvel.com:443/v1/public/comics?'
- const apiKey ='apikey=3a29e41bae5c837ab95a10f9604840ff'
-const char_URL ='https://gateway.marvel.com:443/v1/public/characters?'
 
+
+function callApi() {
+document.querySelector('input').value
+}
 
 function fetchCharacter(charName){
     var queryParams = new URLSearchParams({
         appid: apiKey,
-        name: charName,
+        name: "hulk",
         hash: md5,
         ts: str(time.time()),
-})
-fetch(char_URL + queryParams)
-.then((response)=>response.json())
-.then((response)=>{
-    console.log(response);
+    })
+    fetch(char_URL + queryParams)
+    console.log(char_URL + queryParams)
+    .then((response)=>response.json())
+    .then((response)=>{
+        console.log(response);
     fetchComicthruName(response);
 })}
 
- function fetchComicthruName(charId) {
+function fetchComicthruName(charId) {
     var queryParams = new URLSearchParams({
-    appid: apiKey,
-    character: charId,
+        appid: apiKey,
+        character: charId,
     hash: md5,
     ts: str(time.time()),
-  });
-  fetch(comic_URL + queryParams)
-    .then((response) => response.json())
-    .then((response) => {
-      console.log("response from comic");
-      console.log(response);
-    })
+});
+console.log(comic_URL + queryParams)
+fetch(comic_URL + queryParams)
+.then((response) => response.json())
+.then((response) => {
+    // console.log("response from comic");
+    console.log("response from comic"+response);
+})
  }
-
+ 
  function fetchComic(comic) {
-    var queryParams = new URLSearchParams({
-    appid: apiKey,
+     var queryParams = new URLSearchParams({
+         appid: apiKey,
     title: comic,
     hash: md5,
     ts: str(time.time()),
-  });
+});
   fetch(comic_URL + queryParams)
-    .then((response) => response.json())
+  .then((response) => response.json())
     .then((response) => {
-
+        
       console.log("response from comic");
       console.log(response);
     })
  }
-
+ fetchCharacter()
+ 
+ 
