aaa21a4a61ecdc7cb9fca05a2a85bd3e28f8009f
odupree1988 <odupree1988@gmail.com>
2021-08-03 00:08:16 -0400

    collections and wishlist added

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -2,8 +2,12 @@ const router = require("express").Router();
 const sequelize = require("../config/connection");
 const { User, Comic } = require("../models");
 
-router.get("/", (req, res) => {
-  res.render("dashboard");
+router.get("/collection", (req, res) => {
+  res.render("collection");
+});
+
+router.get("/wishlist", (req, res) => {
+  res.render("wishlist");
 });
 
 module.exports = router;
--- public/javascript/script-api.js
+++ public/javascript/script-api.js
@@ -1,14 +1,12 @@
+let userInput = document.getElementById("input");
+let srchbtn = document.getElementById("srchbtn");
 
-let userInput = document.getElementById("input")
-let srchbtn = document.getElementById("srchbtn")
+srchbtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  let character = userInput.value;
 
- srchbtn.addEventListener('click',function(){
-   let character = userInput.value
-
-   console.log(character)
-   fetch(`http://localhost:3001/api/characters?character=${character}`)
-   .then(res=>res.json())
-   .then(data=>console.log(data))
-
- })
-  
+  console.log(character);
+  fetch(`http://localhost:3001/api/characters?character=${character}`)
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+});
