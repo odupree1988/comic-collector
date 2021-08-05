2c424717ca9894dec65342001814cf77e0ac3801
Salas4969 <82052286+Salas4969@users.noreply.github.com>
2021-08-05 13:02:53 -0400

    render all comics

--- controllers/api/characters-route.js
+++ controllers/api/characters-route.js
@@ -6,6 +6,8 @@ const axios = require("axios");
 const comic_URL = "https://gateway.marvel.com:443/v1/public/comics?";
 const char_URL = "https://gateway.marvel.com:443/v1/public/characters?";
 const { MARVEL_PRIVATE, MARVEL_PUBLIC } = process.env;
+
+
 router.get("/", (req, res) => {
   const { character } = req.query;
 console.log(character)
@@ -16,7 +18,7 @@ console.log(character)
   axios
     .get(`${char_URL}${auth}&nameStartsWith=${character}`)
     .then(({ data }) => {
-      console.log(JSON.stringify(data, null, 2))
+      // console.log(JSON.stringify(data, null, 2))
       let charId = data.data.results[0].id;
     //   if(charId===null){
     //     //   return res.json("please eneter character name differently or character doesn't exist")
@@ -24,8 +26,7 @@ console.log(character)
       axios
         .get(`${comic_URL}${auth}&characters=${charId}`)
         .then(({ data }) => {
-          console.log(JSON.stringify(data, null, 2))
-
+          // console.log(JSON.stringify(data, null, 2))
           return res.json(data);
         })
         .catch((error) => console.log(error));
