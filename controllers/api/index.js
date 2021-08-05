af01b6b1af99b34f6b65b0f9c22256c102e8a032
odupree1988 <odupree1988@gmail.com>
2021-08-05 19:14:24 -0400

    test

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,11 +2,12 @@ const router = require("express").Router();
 
 const userRoutes = require("./user-routes.js");
 const comicRoutes = require("./comic-routes.js");
-const character = require("./characters-route.js")
+const character = require("./characters-route.js");
+const comicRoutes = require("./comicName-route.js");
 
 router.use("/users", userRoutes);
 router.use("/comics", comicRoutes);
-router.use("/characters",character)
-router.use("/comicName",comicName)
+router.use("/characters", character);
+router.use("/comicName", comicName);
 
 module.exports = router;
