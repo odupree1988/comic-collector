4556f797ef597c932d0a2cd682c8416242cd608b
odupree1988 <odupree1988@gmail.com>
2021-08-05 19:52:07 -0400

    merge conflict

--- public/javascript/comic-api.js
+++ public/javascript/comic-api.js
@@ -1,15 +1,13 @@
 let comicInput = document.getElementById("input");
 let comicSrch = document.getElementById("srchbtn");
-
-
 comicSrch.addEventListener("click", function (event) {
-    event.preventDefault();
-    if(document.querySelectorAll("#search").value =="comic"){
+  event.preventDefault();
+  if (document.querySelectorAll("#search").value == "comic") {
     let comicName = comicInput.value;
-  comicName = comicName.replace(/\s+/g,"-")
-  console.log(comicName);
-  fetch(`http://localhost:3001/api/comicName?comic=${comicName}`)
-    .then((res) => res.json())
-    .then((data) => console.log(data));
+    comicName = comicName.replace(/\s+/g, "-");
+    console.log(comicName);
+    fetch(`/api/comicName?comic=${comicName}`)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
   }
 });
